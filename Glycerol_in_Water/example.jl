dd6904b8023fa69d5daa62977d9374f12d2216ec
Leandro Martinez <leandro@iqm.unicamp.br>
2021-09-28 11:49:10 -0300

    up example

--- Glycerol_in_Water/example.jl
+++ Glycerol_in_Water/example.jl
@@ -6,14 +6,21 @@ using EasyFit
 
 # Plot defaults
 plot_font = "Computer Modern"
-default(fontfamily=plot_font,linewidth=2, framestyle=:box, label=nothing, grid=false)
-scalefontsizes()
+default(
+    fontfamily=plot_font,
+    linewidth=2.5, 
+    framestyle=:box, 
+    label=nothing, 
+    grid=false,
+    palette=:tab10
+)
+scalefontsizes(); scalefontsizes(1.3)
 
 system = readPDB("./simulation/equilibrated.pdb")
 
 # The results will be loaded from previous computations. The original
 # code for computing the distributions is commented.
-#trajectory_file = "../trajectories/traj_Glyc.dcd"
+trajectory_file = "../trajectories/traj_Glyc.dcd"
 
 # Select the atoms corresponding to glycerol and water
 glyc = select(system,"resname GLLM")
@@ -22,24 +29,42 @@ water = select(system,"water")
 # Compute glyc-glyc auto correlation 
 solute = Selection(glyc,natomspermol=14)
 #traj = Trajectory(trajectory_file,solute)
-
 #opt = ComplexMixtures.Options(dbulk=20.)
 #mddf_glyc = mddf(traj,opt)
 #save(mddf_glyc,"./results/mddf_glyc.json")
 
+# Compute water-glyc correlation 
+#solvent = Selection(water,natomspermol=3)
+#traj = Trajectory(trajectory_file,solute,solvent)
+#opt = ComplexMixtures.Options(dbulk=20.)
+#mddf_water_glyc = mddf(traj,opt)
+#save(mddf_glyc,"./results/mddf_glyc.json")
+
 # Load previously computed data
 mddf_glyc = load("./results/mddf_glyc.json")
+mddf_water_glyc = load("./results/mddf_water_glyc.json")
 
-# Plot the glycerol auto-correlation function and KB integral
+# Plot the correlation functions and KB integrals
 plot(layout=(2,1))
 x = mddf_glyc.d
 y = movavg(mddf_glyc.mddf,n=10).x
-plot!(x,y,xlabel=L"\textrm{Distance / \AA}",ylabel="MDDF",subplot=1)
-plot!(xlim=(0,20),subplot=1)
+plot!(x,y,label="Glycerol")
+x = mddf_water_glyc.d
+y = movavg(mddf_water_glyc.mddf,n=10).x
+plot!(x,y,label="Water")
+plot!(
+    xlabel=L"\textrm{Distance / \AA}",ylabel="MDDF",
+    xlim=(1.5,8),subplot=1
+)
 
 y = movavg(mddf_glyc.kb,n=10).x
-plot!(x,y,xlabel=L"\textrm{Distance / \AA}",ylabel=L"\textrm{KB~/~cm^3~mol^{-1}}",subplot=2)
-plot!(xlim=(0,20),subplot=2)
+plot!(x,y,subplot=2)
+y = movavg(mddf_water_glyc.kb,n=10).x
+plot!(x,y,subplot=2)
+plot!(
+    xlabel=L"\textrm{Distance / \AA}",ylabel=L"\textrm{KB~/~cm^3~mol^{-1}}",
+    xlim=(0,20),subplot=2
+)
 savefig("./results/mddf_kb.png")
 
 # Plot some group contributions to the MDDF
@@ -49,57 +74,50 @@ aliphatic = ["C1","C2","C3","H11","H12","H2","H31","H32"]
 hydroxyl_contrib = contrib(solute,mddf_glyc.solvent_atom,hydroxyls)
 aliphatic_contrib = contrib(solute,mddf_glyc.solvent_atom,aliphatic)
 
+plot(layout=(2,1))
 x = mddf_glyc.d
 y = movavg(mddf_glyc.mddf,n=10).x
-plot(x,y,label="Total")
+plot!(x,y,label="Total",supblot=1)
 y = movavg(hydroxyl_contrib,n=10).x
-plot!(x,y,label="Hydroxyl contributions")
+plot!(x,y,label="Hydroxyl contributions",subplot=1)
 y = movavg(aliphatic_contrib,n=10).x
-plot!(x,y,label="Aliphatic contributions")
-plot!(
-    xlim=(1,8),
-    xlabel=L"\textrm{Distance / \AA}",
-    ylabel="MDDF"
-)
-savefig("./results/mddf_glyc_groups.png")
-
-# Interactions of glycerol with water
-solvent = Selection(water,natomspermol=3)
-#traj = Trajectory(trajectory_file,solute,solvent)
-#mddf_water_glyc = mddf(traj,opt)
-#save(mddf_water_glyc,"./results/mddf_water_glyc.json")
-mddf_water_glyc = load("./results/mddf_water_glyc.json")
-
-x = mddf_water_glyc.d
-y = movavg(mddf_water_glyc.mddf,n=10).x
-plot(x,y,label="Total",subplot=1)
-
-hydroxyl_contrib = contrib(solute,mddf_water_glyc.solute_atom,hydroxyls)
-aliphatic_contrib = contrib(solute,mddf_water_glyc.solute_atom,aliphatic)
-
-y = movavg(hydroxyl_contrib,n=10).x
-plot!(x,y,label="Hydroxyl contributions")
-y = movavg(aliphatic_contrib,n=10).x
-plot!(x,y,label="Aliphatic contributions")
+plot!(x,y,label="Aliphatic contributions",subplot=1)
 plot!(
     xlim=(1,8),
     xlabel=L"\textrm{Distance / \AA}",
     ylabel="MDDF",
+    subplot=1
 )
-savefig("./results/mddf_water_glyc.png")
+
+x = mddf_water_glyc.d
+y = movavg(mddf_water_glyc.mddf,n=10).x
+plot!(x,y,label="Total",subplot=2)
+hydroxyl_contrib = contrib(solute,mddf_water_glyc.solute_atom,hydroxyls)
+aliphatic_contrib = contrib(solute,mddf_water_glyc.solute_atom,aliphatic)
+
+y = movavg(hydroxyl_contrib,n=10).x
+plot!(x,y,label="Hydroxyl contributions",subplot=2)
+y = movavg(aliphatic_contrib,n=10).x
+plot!(x,y,label="Aliphatic contributions",subplot=2)
+plot!(
+    xlim=(1,8),
+    ylabel="MDDF",
+    subplot=2
+)
+savefig("./results/mddf_groups.png")
 
 # 2D plot of group contributions
 groups = [
-    select(system,"name O1 HO1"), # first hydroxyl
-    select(system,"name C1 H11 H12"), # first CH2
-    select(system,"name O2 HO2"), # second hydroxyl
-    select(system,"name C2 H2"), # CH
-    select(system,"name C3 H31 H32"), # second CH2
-    select(system,"name O3 OH3") # third hydroxyl
+    (["O1","HO1"],"OH"), # first hydroxyl
+    (["C1","H11","H12"],L"\textrm{CH_2}"), # first CH2
+    (["O2","HO2"],"OH"), # second hydroxyl
+    (["C2","H2"],"CH"), # CH
+    (["C3","H31","H32"],L"\textrm{CH_2}"), # second CH2
+    (["O3","HO3"],"OH") # third hydroxyl
 ] 
 group_contrib = zeros(length(mddf_glyc.d),length(groups))
 for (igroup, group) in pairs(groups)
-    group_contrib[:,igroup] .= contrib(solute,mddf_glyc.solute_atom,group)
+    group_contrib[:,igroup] .= contrib(solute,mddf_glyc.solute_atom,group[1])
 end
 
 idmin = findfirst( d -> d > 1.5, mddf_glyc.d)
@@ -116,24 +134,3 @@ contourf(
     xticks=(1:length(groups),labels),xrotation=60,
 )
 savefig("./results/map2D_glyc_glyc.png")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
