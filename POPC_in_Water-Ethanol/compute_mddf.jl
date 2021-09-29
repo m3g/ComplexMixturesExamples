20a47baed62d3dc0b50c6d86f64672dc4066e2de
Leandro Martinez <leandro@iqm.unicamp.br>
2021-09-29 10:32:25 -0300

    updated comments

--- POPC_in_Water-Ethanol/compute_mddf.jl
+++ POPC_in_Water-Ethanol/compute_mddf.jl
@@ -13,13 +13,20 @@ popc = select(system,"resname POPC")
 water = select(system,"water")
 ethanol = select(system,"resname ETOH")
 
-# Set the complete membrane as the solute
+# Set the complete membrane as the solute. We use nmols=1 here such
+# that the membrane is considered a single solute in the calculation. 
 solute = Selection(popc,nmols=1)
 
 # Compute water-POPC distribution and KB integral 
 solvent = Selection(water,natomspermol=3)
 traj = Trajectory(trajectory_file,solute,solvent)
-opt = ComplexMixtures.Options(dbulk=10.,lastframe=200)
+
+# We want to get reasonably converged KB integrals, which usually
+# require large solute domains. Distribution functions converge 
+# rapidly (~10Angs or less), on the other side.
+opt = ComplexMixtures.Options(dbulk=20.)
+
+# Compute the MDDF
 mddf_water_POPC = mddf(traj,opt)
 
 # Save results for later use
@@ -28,8 +35,9 @@ save(mddf_water_POPC,"./results/mddf_water_POPC.json")
 # Compute water-POPC distribution and KB integral 
 solvent = Selection(ethanol,natomspermol=9)
 traj = Trajectory(trajectory_file,solute,solvent)
-opt = ComplexMixtures.Options(dbulk=10.,lastframe=200)
+opt = ComplexMixtures.Options(dbulk=20.)
 mddf_ethanol_POPC = mddf(traj,opt)
 
 # Save results for later use
 save(mddf_ethanol_POPC,"./results/mddf_ethanol_POPC.json")
+
