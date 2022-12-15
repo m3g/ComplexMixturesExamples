273a99a7671be76606437355333b2dd162356fc9
Leandro Martinez <lmartine@unicamp.br>
2022-12-15 08:52:29 -0300

    render 3D figure

--- Test/test.jl
+++ Test/test.jl
@@ -5,21 +5,20 @@
 # of the repository. Checking the differences to the main branch allows
 # visualization of the differences in the resulting images.
 #
-using ComplexMixtures
-script_dir = @__DIR__
+test_dir = @__DIR__
 
 #
 # Download trajectory files
 #
-found_all_downloads = isfile("$script_dir/../Test/trajectories/traj_Glyc.dcd")
-found_all_downloads = isfile("$script_dir/../Test/trajectories/traj_POPC.dcd")
-found_all_downloads = isfile("$script_dir/../Test/trajectories/traj_Polyacry.dcd")
-found_all_downloads = isfile("$script_dir/../Test/trajectories/glyc50.dcd")
+found_all_downloads = isfile("$test_dir/../Test/trajectories/traj_Glyc.dcd")
+found_all_downloads = isfile("$test_dir/../Test/trajectories/traj_POPC.dcd")
+found_all_downloads = isfile("$test_dir/../Test/trajectories/traj_Polyacry.dcd")
+found_all_downloads = isfile("$test_dir/../Test/trajectories/glyc50.dcd")
 
 if !found_all_downloads
     error("""
 
-    Missing full trajectory files at $script_dir/trajectories
+    Missing full trajectory files at $test_dir/trajectories
     
     Download file (200Mb): https://drive.google.com/file/d/1BuXJ8AjBeduMSD2CkDJLDNxAAD2QNNg6/view?usp=sharing
     Download file (280Mb): https://drive.google.com/file/d/1ug43ncCLsBATaJrT9zlbaqK6AORVvhhx/view?usp=sharing
@@ -28,4 +27,29 @@ if !found_all_downloads
 
     """)
 end
+
+# Glycerol in water
+dir = "$test_dir/../Glycerol_in_Water"
+include("$dir/compute_mddf.jl")
+include("$dir/plots.jl")
+
+# Polyacrylamide in DMF
+dir = "$test_dir/../Polyacrylamide_in_DMF"
+include("$dir/compute_mddf.jl")
+include("$dir/plots.jl")
+
+# POPC in Water/Ethanol
+dir = "$test_dir/../POPC_in_Water-Ethanol"
+include("$dir/compute_mddf.jl")
+include("$dir/plots.jl")
+
+# Protein in Glycerol 
+dir = "$test_dir/../Protein_in_Glycerol"
+include("$dir/MDDF/mddf.jl")
+include("$dir/Density2D/density2D.jl")
+include("$dir/Density3D/density3D.jl")
+
+
+
+
 
